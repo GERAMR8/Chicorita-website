e9ce41689c460a803a9ecd5625eaa09043db5033
gerardomr8 <gerardo.mixteco@quadit.mx>
2021-01-12 22:06:55 -0600

    primmer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<!--
+	Intensify by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Internet Papeleria Luna</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets2/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<nav class="left">
+					<a href="#menu"><span>Menu</span></a>
+				</nav>
+				<a href="index.html" class="logo">Internet Papeleria Luna</a>
+				<nav class="right">
+					<a href="{{('/login')}}" class="button alt">Iniciar sesión</a>
+				</nav>
+			</header>
+
+		<!-- Menu -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Inicio</a></li>
+					<li><a href="generic.html">Planes</a></li>
+					<li><a href="elements.html">Contacto</a></li>
+				</ul>
+				<ul class="actions vertical">
+					<li><a href="#" class="button fit">Login</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="content">
+					<h1>Bienvenido</h1>
+					<p><b>Consulta nuestros planes de internet y animaté a unirte a nuestro sistema.</b></p>
+					<ul class="actions">
+						<li><a href="#one" class="button scrolly">Consulta</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper">
+				<div class="inner flex flex-3">
+					<div class="flex-item left">
+						<div>
+							<h3>Planes desde $200</h3>
+							<p>Siempre con <br> un sistema de internet estable.</p>
+						</div>
+						<div>
+							<h3>100% confiable</h3>
+							<p>El pago lo realizas <br>y en ese instante tienes internet</p>
+						</div>
+					</div>
+					<div class="flex-item image fit round">
+						<img src="images/pic01.jpg" alt="" />
+					</div>
+					<div class="flex-item right">
+						<div>
+							<h3>Accesible</h3>
+							<p>No te preopes<br/> Nos adaptamos a tu presupuesto.</p>
+						</div>
+						<div>
+							<h3>Suscribete</h3>
+							<p>Para mas informacion <br /> llama al siguinente numero: <br> 272 719 4597.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<h2>Contactanos en: </h2>
+					<ul class="actions">
+						<li><span class="icon fa-phone"></span> <a href="#">(272) 719 4597</a></li>
+						<li><span class="icon fa-envelope"></span> <a href="#">papelerialuna1997@gmail.com</a></li>
+						<li><span class="icon fa-map-marker"></span> Dos Ríos, Veracruz.</li>
+					</ul>
+				</div>
+				<div class="copyright">
+          &copy; Mix-Solutions-GMR</a>.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets2/js/jquery.min.js"></script>
+			<script src="assets2/js/jquery.scrolly.min.js"></script>
+			<script src="assets2/js/skel.min.js"></script>
+			<script src="assets2/js/util.js"></script>
+			<script src="assets2/js/main.js"></script>
+
+	</body>
+</html>
